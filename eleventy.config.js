fbf5fc109a2e7dabef8914a986a364d21519c077
IgnacioAntGP <ignaa.gonzalezp@duocuc.cl>
2026-09-13 20:34:08 -0300

    feat: añadido proyectos adicionales al html y a configuraciones

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,18 @@
 import { EleventyRenderPlugin } from "@11ty/eleventy";
 
 export default function(eleventyConfig) {
-  // Copiar tus imágenes y estilos directamente a la carpeta final
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/proyectos/wallet/**/*.js");
-  eleventyConfig.addPassthroughCopy("src/proyectos/wallet/**/*.css");
+  //Estilos internos de los proyectos
+  eleventyConfig.addPassthroughCopy("src/proyectos/**/*.js");
+  eleventyConfig.addPassthroughCopy("src/proyectos/**/*.css");
 
   return {
     dir: {
-      input: "src",          // Tu carpeta de desarrollo
-      output: "dist",        // Tu carpeta de producción final
-      includes: "templates"  // Donde buscará tus layouts (relativo a 'src')
+      input: "src",        
+      output: "dist",
+      includes: "templates" // Plantillas .html
     },
-    // Forzamos a que use Nunjucks para procesar tus archivos HTML planos
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
   };
