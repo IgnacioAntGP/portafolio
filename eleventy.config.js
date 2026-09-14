b614d93bea3bbbc824ccd86425ec3bce4df5dcb7
IgnacioAntGP <ignaa.gonzalezp@duocuc.cl>
2026-09-13 22:53:34 -0300

    fix: path para gitpage

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,14 @@
-import { EleventyRenderPlugin } from "@11ty/eleventy";
-
-export default function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/css");
-  //Estilos internos de los proyectos
   eleventyConfig.addPassthroughCopy("src/proyectos/**/*.js");
   eleventyConfig.addPassthroughCopy("src/proyectos/**/*.css");
 
   return {
+    pathPrefix: "/portafolio/",
     dir: {
-      input: "src",        
+      input: "src",
       output: "dist",
-      includes: "templates" // Plantillas .html
+      includes: "templates"
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
